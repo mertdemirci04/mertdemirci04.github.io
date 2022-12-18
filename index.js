6d0e29a24f2d7baf8b59fe77d90aedf362600ebe
mertdemirci04 <114282417+mertdemirci04@users.noreply.github.com>
2022-12-18 11:43:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const navigation = document.querySelector('.navigation')
+    document.querySelector(".toggle").onclick = function(){
+        this.classList.toggle('active');
+        navigation.classList.toggle('active');
+    };
 
+const burger = document.querySelector(".li2");
+const sidebar = document.querySelector(".sidebar");
+
+burger.addEventListener("click",function(){
+    sidebar.classList.toggle('active');
+    sidebar.style.display = "block"
+})
